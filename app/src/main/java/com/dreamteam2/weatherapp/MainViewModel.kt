c28bd6d91fd373ecfffc16eeb9a8b16e5dd7d616
CarterGrove24 <cgrove001@csbsju.edu>
2022-11-23 22:29:34 -0600

    Refactor MainViewModel
    Moved all api calls out of init function into different suspend functions.

--- app/src/main/java/com/dreamteam2/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/dreamteam2/weatherapp/MainViewModel.kt
@@ -18,6 +18,14 @@ class MainViewModel: ViewModel() {
 
     private val weatherApi = WeatherApi()
 
+    private var searchCity: String = "St. Joseph"
+    private var searchState: String = "Minnesota"
+    private var lat: Double = 45.5648
+    private var long: Double = -94.3180
+    private var cwa: String = "MPX"
+    private var gridX: Int = 72
+    private var gridY: Int = 98
+
     val status = MutableStateFlow<Status?>(null)
     val gridPointEndpoints = MutableStateFlow<GridPointEndpoints?>(null)
     val forecast = MutableStateFlow<Forecast?>(null)
@@ -27,66 +35,76 @@ class MainViewModel: ViewModel() {
     private val coordinatesApi = CoordinatesAPI()
     val coordinates = MutableStateFlow<List<CoordinatesData>?>(null)
 
-
-    init{
-        //Launches viewmodel with a call to the WeatherAPI that fetches the standard generic output of the API
-        viewModelScope.launch {
-            kotlin.runCatching {
-               // weatherApi.getForecast(31, 80)
-                weatherApi.getGirdEndpoints(45.5648,-94.3180) // 39.0473 -95.6752 vs 45.5648 -94.3180
-            }.onSuccess {
-                gridPointEndpoints.value = it
-            }.onFailure {
-                gridPointEndpoints.value = null
-            }
-
-        //
-            kotlin.runCatching {
-                weatherApi.getForecast("MPX", 72, 98) // TOP 31 80 vw MPX 72 98
-            }.onSuccess {
-                forecast.value = it
-            }.onFailure {
-                forecast.value = null
-            }
-
-        //Call to WeatherAPI which fetches the hourly forecast
-            kotlin.runCatching {
-                weatherApi.getForecastHourly("MPX", 72, 98)
-            }.onSuccess {
-                forecastHourly.value = it
-            }.onFailure {
-                forecastHourly.value = null
-            }
-
-        //Call to WeatherAPI that gets the properties of a gridpoint
-            kotlin.runCatching {
-                weatherApi.getGridpointProperties("MPX", 72, 98)
-            }.onSuccess {
-                gridpointsProperties.value = it
-            }.onFailure {
-                gridpointsProperties.value = null
-            }
-
-            //Call to CoordinatesAPI for the city and state
-            kotlin.runCatching {
-                coordinatesApi.getCoordinates("Willmar", "Minnesota")
-            }.onSuccess {
-                coordinates.value = it
-            }.onFailure {
-                coordinates.value = null
-            }
-
-            //Call to WeatherAPI for the current status of the API
-            kotlin.runCatching {
-                weatherApi.getStatus()
-            }.onSuccess {
-                status.value = it
-            }.onFailure {
-                status.value = null
-            }
-
-
+    suspend fun getStatus(){
+        //Call to WeatherAPI for the current status of the API
+        kotlin.runCatching {
+            weatherApi.getStatus()
+        }.onSuccess {
+            status.value = it
+        }.onFailure {
+            status.value = null
         }
+    }
+
+    suspend fun getGridEndpoints(){
+        kotlin.runCatching {
+            weatherApi.getGirdEndpoints(this.long, this.lat) // 39.0473 -95.6752 vs 45.5648 -94.3180
+        }.onSuccess {
+            gridPointEndpoints.value = it
+        }.onFailure {
+            gridPointEndpoints.value = null
+        }
+    }
+
+    suspend fun getDailyForecast(){
+        kotlin.runCatching {
+            weatherApi.getForecast(this.cwa, this.gridX, this.gridY) // TOP 31 80 vw MPX 72 98
+        }.onSuccess {
+            forecast.value = it
+        }.onFailure {
+            forecast.value = null
+        }
+    }
+
+    suspend fun getHourlyForecast(){
+        //Call to WeatherAPI which fetches the hourly forecast
+        kotlin.runCatching {
+            weatherApi.getForecastHourly(this.cwa, this.gridX, this.gridY)
+        }.onSuccess {
+            forecastHourly.value = it
+        }.onFailure {
+            forecastHourly.value = null
+        }
+    }
+
+    suspend fun getGridpointProperties(){
+        //Call to WeatherAPI that gets the properties of a gridpoint
+        kotlin.runCatching {
+            weatherApi.getGridpointProperties(this.cwa, this.gridX, this.gridY)
+        }.onSuccess {
+            gridpointsProperties.value = it
+        }.onFailure {
+            gridpointsProperties.value = null
+        }
+    }
+
+    suspend fun getCoordinates(){
+        //Call to CoordinatesAPI for the city and state
+        kotlin.runCatching {
+            coordinatesApi.getCoordinates(this.searchCity, this.searchState)
+        }.onSuccess {
+            coordinates.value = it
+        }.onFailure {
+            coordinates.value = null
+        }
+    }
+
+    suspend fun setSearchCity(newString: String){
+        this.searchCity = newString
+    }
+
+    suspend fun setSearchState(newString: String){
+        this.searchState = newString
     }
 
 }
